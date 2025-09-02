bc4704e98f5cd0d5c0f2cab34c167ed997f8619a
Nevroz Arslan <nevrozarslan@gmail.com>
2025-09-02 13:01:16 +0300

    Add License

--- mix.exs
+++ mix.exs
@@ -1,6 +1,12 @@
 defmodule Mqttc.MixProject do
   use Mix.Project
 
+  @description "MQTT v5 Client for Elixir."
+
+  @repo_url "https://github.com/narslan/mqttc"
+
+  @version "0.1.0"
+
   def project do
     [
       app: :mqttc,
@@ -8,7 +14,21 @@ defmodule Mqttc.MixProject do
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
-      deps: deps()
+      deps: deps(),
+
+       # Hex
+      package: package(),
+      description: @description
+      name: "Mqttc",
+      docs: [
+        main: "Mqttc",
+        source_ref: "v#{@version}",
+        source_url: @repo_url,
+        extras: [
+          "README.md",
+          "LICENSE.txt": [title: "License"]
+        ]
+
     ]
   end
 
@@ -16,6 +36,14 @@ defmodule Mqttc.MixProject do
   def application do
     [
       extra_applications: [:ssl, :logger]
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Nevroz Arslan"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url}
     ]
   end
 
